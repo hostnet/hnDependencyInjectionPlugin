06e77c0c5ff6bf17f69d221c1b4603adfb90d83d
Nathan Utama <nathan@nutama.nl>
2020-10-14 16:34:45 +0200

    fixed lowest build compatibility

--- test/Symfony1FallbackTest.php
+++ test/Symfony1FallbackTest.php
@@ -6,6 +6,7 @@ use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\Event\KernelEvent;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
@@ -153,7 +154,7 @@ class Symfony1FallbackTest extends TestCase
         );
     }
 
-    private function buildResponseEvent($ex): RequestEvent
+    private function buildResponseEvent($ex): GetResponseEvent
     {
         return new GetResponseForExceptionEvent(
             $this->createMock(HttpKernelInterface::class),
