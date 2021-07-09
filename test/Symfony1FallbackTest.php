ab19ffc21a395a5b0bedb798dd89321708ec803f
Nathan Utama <nathan@nutama.nl>
2021-07-09 13:17:50 +0200

    added support for symfony 5

--- test/Symfony1FallbackTest.php
+++ test/Symfony1FallbackTest.php
@@ -7,8 +7,6 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
-use Symfony\Component\HttpKernel\Event\FilterControllerEvent;
-use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 /**
